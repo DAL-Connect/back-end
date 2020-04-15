6f7352f0b4d248213854b1354a132ae6e37a4f24
Brittany Houtz <brittany.houtz@gmail.com>
2020-04-15 14:25:03 -0700

    some scaffolding

---
+++ models/Island.js
@@ -0,0 +1,9 @@
+// creation time
+// closing time
+// fruits: sharing (bool) + types ([str])
+// hemisphere (str)
+// turnips (num)
+// shops ([str])
+// host (str)
+// dodo code (str)
+// guests ([str])
